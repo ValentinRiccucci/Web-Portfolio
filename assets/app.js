98bda008efc78e1c0db50d8353ffce97a49f5145
Valentin RICCUCCI <84391481+ValentinRiccucci@users.noreply.github.com>
2026-03-08 00:34:03 +0100

    Add cursor decoration

--- assets/app.js
+++ assets/app.js
@@ -1,4 +1,4 @@
-import './bootstrap.js';
+import './stimulus_bootstrap.js';
 /*
  * Welcome to your app's main JavaScript file!
  *
@@ -8,3 +8,35 @@ import './bootstrap.js';
 import './styles/app.css';
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
+
+// import './JS/ASCII.js'
+// import './JS/Animations.js'
+
+const chars = "!@#$%^&*";
+
+
+
+let last = 0;
+document.addEventListener("mousemove", (e) => {
+
+
+    const now = Date.now();
+    if(now - last < 60) return;
+
+    last = now;
+
+    const span = document.createElement("span");
+    span.classList.add("ascii-char");
+
+    span.textContent = chars[Math.floor(Math.random() * chars.length)];
+
+    span.style.left = e.pageX + "px";
+    span.style.top = e.pageY + "px";
+
+    document.body.appendChild(span);
+
+    setTimeout(() => {
+        span.remove();
+    }, 1000);
+
+});
